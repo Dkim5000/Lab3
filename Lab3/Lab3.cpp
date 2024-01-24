8f27ac90204b15aa5a14cc977ce6f9acbeb08475
Daniel Taejoon Kim <kimdt@purdue.edu>
2024-01-24 12:48:07 -0500

    Math

--- Lab3/Lab3.cpp
+++ Lab3/Lab3.cpp
@@ -7,9 +7,10 @@ using namespace std;
 
 void printMenu(int& choice) {
 	cout << "Welcome to my program, please select an operation to perform:" << endl;
-	cout << "1. Placeholder" << endl;
-	cout << "2. Another Placeholder" << endl;
-	cout << "3. And a third one" << endl;
+	cout << "1. Addition" << endl;
+	cout << "2. Subtraction" << endl;
+	cout << "3. Multiplication" << endl;
+	cout << "4. Division" << endl;
 
 	cout << "\nYour Selection: ";
 	cin >> choice;
@@ -17,15 +18,25 @@ void printMenu(int& choice) {
 void getChoices(float& A, float& B) {
 	cout << "Please enter the first value:";
 	cin >> A;
-	// The rest of this function is an exercise to the reader
+	cout << "Please enter the second value:";
+	cin >> B;
+	cout << "" << endl;
 }
 
-void firstChoice(float A, float B) {
-	cout << "This is proving the first choice ran" << endl;
+void firstChoice(float A, float B, float y) {
+	cout << A << " + " << B << " = " << y << endl;
 }
 
-void secondChoice(float A, float B) {
-	cout << "This is proving the second choice ran" << endl;
+void secondChoice(float A, float B, float y) {
+	cout << A << " - " << B << " = " << y << endl;
+}
+
+void thirdChoice(float A, float B, float y) {
+	cout << A << " * " << B << " = " << y << endl;
+}
+
+void fourthChoice(float A, float B, float y) {
+	cout << A << " / " << B << " = " << y << endl;
 }
 
 int main() {
@@ -33,16 +44,29 @@ int main() {
 	int choice;
 	float A;
 	float B;
+	float y;
 
 	printMenu(choice);
 	getChoices(A, B);
 
 	if (choice == 1) {
-		firstChoice(A, B);
+		y = A + B;
+		firstChoice(A, B, y);
 	}
 
 	if (choice == 2) {
-		secondChoice(A, B);
+		y = A - B;
+		secondChoice(A, B, y);
+	}
+
+	if (choice == 3) {
+		y = A * B;
+		thirdChoice(A, B, y);
+	}
+
+	if (choice == 4) {
+		y = A / B;
+		fourthChoice(A, B, y);
 	}
 
 	return 0;
